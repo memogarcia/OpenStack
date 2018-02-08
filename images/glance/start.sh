5daaf2509087ef0dc637788b549c426f65e84466
Memo García <sirmemogarcia@gmail.com>
2018-02-08 11:52:32 +0100

    glance

--- images/glance/start.sh
+++ images/glance/start.sh
@@ -18,22 +18,39 @@ glance-manage db_sync
 source /opt/osrc-v3
 
 echo "Creating user"
-openstack user create --domain Default --password secret glance
+if [ -z `openstack user list -f csv -q |grep glance` ]
+then
+    openstack user create --domain Default --password secret glance
+else
+    echo "Skipping"
+fi
+
 
 echo "Creating role"
 openstack role add --project service --user glance admin
 
 echo "Creating glance service"
-openstack service create --name glance --description "OpenStack Image" image
+if [ -z `openstack service list -f csv -q |grep glance` ]
+then
+    openstack service create --name glance --description "OpenStack Image" image
+else
+    echo "Skipping"
+fi
 
-echo "Creating public endpoint"
-openstack endpoint create --region RegionOne image public http://0.0.0.0:9292
+endpoint=`openstack endpoint list -f csv -q |grep glance`
+if [ -z "$endpoint" ]
+then
+    echo "Creating public endpoint"
+    openstack endpoint create --region RegionOne image public http://0.0.0.0:9292
 
-echo "Creating internal endpoint"
-openstack endpoint create --region RegionOne image internal http://0.0.0.0:9292
+    echo "Creating internal endpoint"
+    openstack endpoint create --region RegionOne image internal http://0.0.0.0:9292
 
-echo "Creating admin endpoint"
-openstack endpoint create --region RegionOne image admin http://0.0.0.0:9292
+    echo "Creating admin endpoint"
+    openstack endpoint create --region RegionOne image admin http://0.0.0.0:9292
+else
+    echo "Skipping"
+fi
 
 echo "Starting glance api's"
 glance-api &
