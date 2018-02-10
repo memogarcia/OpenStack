aec017c9c1c3e38e73d2558653867262d974444b
Memo García <sirmemogarcia@gmail.com>
2018-02-10 21:09:59 +0100

    opendaylight 2

---
+++ images/opendaylight/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -u
+
+echo "Running opendaylight server"
+bin/start
+
+echo "Running opendaylight client"
+bin/client feature:install odl-netvirt-openstack odl-dlux-core odl-mdsal-apidocs
