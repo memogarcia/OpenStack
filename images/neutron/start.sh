c72d97b7a3a7a3f690c3c78b65b1ed6e0134815d
Memo García <sirmemogarcia@gmail.com>
2018-02-10 19:36:17 +0100

    neutron conf

--- images/neutron/start.sh
+++ images/neutron/start.sh
@@ -13,3 +13,44 @@ mysql  -hopenstack_mariadb -umysql -psecret \
 mysql  -hopenstack_mariadb -umysql -psecret \
        -e "GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'%' \
         IDENTIFIED BY 'secret'"
+
+source /opt/osrc-v3
+
+echo "Creating user"
+if [ -z `openstack user list -f csv -q |grep neutron` ]
+then
+    openstack user create --domain Default --password secret neutron
+else
+    echo "Skipping"
+fi
+
+echo "Creating role"
+openstack role add --project service --user neutron admin
+
+echo "Creating neutron service"
+if [ -z `openstack service list -f csv -q |grep neutron` ]
+then
+    openstack service create --name neutron --description "OpenStack Networking" network
+else
+    echo "Skipping"
+fi
+
+endpoint=`openstack endpoint list -f csv -q |grep neutron`
+if [ -z "$endpoint" ]
+then
+    echo "Creating public endpoint"
+    openstack endpoint create --region RegionOne image public http://openstack_neutron:9696
+
+    echo "Creating internal endpoint"
+    openstack endpoint create --region RegionOne image internal http://openstack_neutron:9696
+
+    echo "Creating admin endpoint"
+    openstack endpoint create --region RegionOne image admin http://openstack_neutron:9696
+else
+    echo "Skipping"
+fi
+
+echo "Create databases tables"
+neutron-db-manage --config-file /etc/neutron/neutron.conf \
+                        --config-file /etc/neutron/plugins/ml2/ml2_conf.ini \
+                        upgrade head
