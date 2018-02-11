99edc5c36da431d26a4c7a915d32576bedf750be
Memo García <sirmemogarcia@gmail.com>
2018-02-11 04:40:55 +0100

    swift

--- images/opendaylight/README.md
+++ images/opendaylight/README.md
@@ -2,6 +2,8 @@
 
 ## How to deploy it on docker
 
+    ovs-vsctl set-manager tcp:openstack_neutron:6640
+
 ## References
 
 [opendaylight and openstack](http://docs.opendaylight.org/en/stable-carbon/submodules/netvirt/docs/openstack-guide/openstack-with-netvirt.html#installing-opendaylight-on-an-existing-openstack)
---
+++ images/swift/start.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -u
+
+echo "DB configuration"
+mysql  -hopenstack_mariadb -uroot -psecret \
+    -e "CREATE DATABASE cinder;"
+
+mysql  -hopenstack_mariadb -uroot -psecret \
+       -e "GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'localhost' \
+        IDENTIFIED BY 'secret';"
+
+mysql  -hopenstack_mariadb -uroot -psecret \
+       -e "GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'%' \
+        IDENTIFIED BY 'secret'"
+
+echo "Syncing db"
+cinder-manage db sync
+
+source /opt/osrc-v3
+
+echo "Creating user"
+if [ -z `openstack user list -f csv -q |grep swift` ]
+then
+    openstack user create --domain Default --password secret swift
+else
+    echo "Skipping"
+fi
+
+
+echo "Creating role"
+openstack role add --project service --user swift admin
+
+echo "Creating cinder service"
+if [ -z `openstack service list -f csv -q |grep cinder` ]
+then
+    openstack service create --name swift --description "OpenStack Object Storage" object-store
+else
+    echo "Skipping"
+fi
+
+endpoint=`openstack endpoint list -f csv -q |grep cinder`
+if [ -z "$endpoint" ]
+then
+    echo "Creating public endpoint"
+    openstack endpoint create --region RegionOne object-store public http://openstack_swift:8081/v1/AUTH_%\(project_id\)s
+
+    echo "Creating internal endpoint"
+    openstack endpoint create --region RegionOne object-store internal http://openstack_swift:8081/v1/AUTH_%\(project_id\)s
+
+    echo "Creating admin endpoint"
+    openstack endpoint create --region RegionOne object-store admin http://openstack_swift:8081/v1/AUTH_%\(project_id\)s
+else
+    echo "Skipping"
+fi
+
+echo "Starting supervisord"
+supervisord
