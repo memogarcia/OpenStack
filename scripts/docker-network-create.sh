0673239240875cf703e670a2368708cfac42c0eb
Memo García <sirmemogarcia@gmail.com>
2018-02-13 23:48:44 +0100

    swift 6

--- scripts/docker-network-create.sh
+++ scripts/docker-network-create.sh
@@ -2,7 +2,7 @@
 
 set -u -x
 
-for network in openstack-management-net openstack-public-net openstack-private-net
+for network in openstack-management-net openstack-public-net openstack-private-net openstack-swift-net
 do
     docker network create "$network"
 done
---
+++ services/openstack/swift/README.md
@@ -0,0 +1,6 @@
+# Openstack swift
+
+It doesn't use the same database as the rest of the openstack servers.
+
+liberasurecode configuration (find the error again)
+
