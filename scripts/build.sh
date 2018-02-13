2bb0cb714e335d2e11c91d71fce128d1b692c829
Memo García <sirmemogarcia@gmail.com>
2018-02-13 13:59:13 +0100

    Readme

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,16 +6,16 @@ OPENSTACK_SERVICES=$(cat scripts/openstack_services.txt)
 THIRD_PARTY_SERVICES=$(cat scripts/third_party_services.txt)
 
 echo "Building the seed image"
-docker build -t openstack/seed:latest services/third-party/seed
+docker build -t openstack/seed:latest services/third-party/seed --no-cache
 
 echo "Building third party services"
 for service in $THIRD_PARTY_SERVICES
 do
-    docker-compose -f services/third-party/$service/dev.yml build
+    docker-compose -f services/third-party/$service/dev.yml build --no-cache
 done
 
 echo "Building OpenStack services"
 for service in $OPENSTACK_SERVICES
 do
-    docker-compose -f services/openstack/$service/dev.yml build
+    docker-compose -f services/openstack/$service/dev.yml build --no-cache
 done
---
+++ services/third-party/openldap/README.md
@@ -0,0 +1,5 @@
+# OpenLDAP
+
+## References
+
+[integrate openldap](https://heig-cloud.github.io/article/2015-12-17%20ldap/)
