94ce98392cae599c6611e5537026f875cf8c4a2b
Memo García <sirmemogarcia@gmail.com>
2018-02-14 01:24:05 +0100

    volume 2

--- scripts/docker-volume-create.sh
+++ scripts/docker-volume-create.sh
@@ -11,3 +11,13 @@ for service in $ALL_SERVICES
 do
     docker volume create "openstack_volume_$service"
 done
+
+# what happens if you rerun this command with the data in the volume?
+for service in $OPENSTACK_SERVICES
+do
+    docker volume create "openstack_volume_$service_etc"
+    docker volume create "openstack_volume_$service_lib"
+    docker volume create "openstack_volume_$service_log"
+done
+
+# the problem with external volumes is that they are not portable, but I guess is the same for networks.
