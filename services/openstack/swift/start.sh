a21f8d8d5de197ae8afafa7c797b25bcfd693e76
Memo García <sirmemogarcia@gmail.com>
2018-02-15 21:38:54 +0100

    stable/swift

--- services/openstack/swift/start.sh
+++ services/openstack/swift/start.sh
@@ -2,6 +2,25 @@
 
 set -u
 
+echo "Applying configuration"
+cp /opt/swift/etc/account-server.conf-sample /etc/swift/account-server.conf
+cp /opt/swift/etc/container-reconciler.conf-sample /etc/swift/container-reconciler.conf
+cp /opt/swift/etc/container-server.conf-sample /etc/swift/container-server.conf
+cp /opt/swift/etc/container-sync-realms.conf-sample /etc/swift/container-sync-realms.conf
+cp /opt/swift/etc/dispersion.conf-sample /etc/swift/dispersion.conf
+cp /opt/swift/etc/internal-client.conf-sample /etc/swift/internal-client.conf
+cp /opt/swift/etc/keymaster.conf-sample /etc/swift/keymaster.conf
+cp /opt/swift/etc/memcache.conf-sample /etc/swift/memcache.conf
+cp /opt/swift/etc/mime.types-sample /etc/swift/mime.types
+cp /opt/swift/etc/object-expirer.conf-sample /etc/swift/object-expirer.conf
+cp /opt/swift/etc/object-server.conf-sample /etc/swift/object-server.conf
+cp /opt/swift/etc/rsyncd.conf-sample /etc/swift/rsyncd.conf
+cp /opt/swift/etc/swift-rsyslog.conf-sample /etc/swift/swift-rsyslog.conf
+cp /opt/swift/etc/swift.conf-sample /etc/swift/swift.conf
+
+RUN python2 /opt/configparse.py --config /opt/config/config-proxy.json --service "/etc/swift/proxy-server.conf"
+RUN python2 /opt/configparse.py --config /opt/config/config.json --service "/etc/swift/swift.conf"
+
 echo "DB configuration"
 mysql  -hopenstack_mariadb -uroot -psecret \
     -e "CREATE DATABASE swift;"
@@ -14,9 +33,6 @@ mysql  -hopenstack_mariadb -uroot -psecret \
        -e "GRANT ALL PRIVILEGES ON swift.* TO 'swift'@'%' \
         IDENTIFIED BY 'secret'"
 
-echo "Syncing db"
-# cinder-manage db sync
-
 source /opt/osrc-v3
 
 echo "Creating user"
@@ -26,7 +42,6 @@ then
 else
     echo "Skipping"
 fi
-
 
 echo "Creating role"
 openstack role add --project service --user swift admin
