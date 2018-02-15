2515ba801d36fac9e07b0bc754209cfc4e25bce2
Memo García <sirmemogarcia@gmail.com>
2018-02-15 21:45:31 +0100

    stable/qemu

--- services/openstack/nova-qemu/start.sh
+++ services/openstack/nova-qemu/start.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-# set -u
+set -u
+
+echo "Applying configuration"
+
+cp /opt/nova/etc/nova/api-paste.ini etc/nova/api-paste.ini
+cp /opt/nova/etc/nova/cells.json /etc/nova/cells.json
+cp /opt/nova/etc/nova/logging_sample.conf /etc/nova/logging.conf
+cp /opt/nova/etc/nova/nova-config-generator.conf /etc/nova/nova-config-generator.conf
+cp /opt/nova/etc/nova/nova-policy-generator.conf /etc/nova/nova-policy-generator.conf
+cp /opt/nova/etc/nova/rootwrap.conf /etc/nova/rootwrap.conf
+cp /opt/nova/etc/nova/rootwrap.d/api-metadata.filters /etc/nova/rootwrap.d/api-metadata.filters
+cp /opt/nova/etc/nova/rootwrap.d/compute.filters /etc/nova/rootwrap.d/compute.filters
+cp /opt/nova/etc/nova/rootwrap.d/network.filters /etc/nova/rootwrap.d/network.filters
+
+RUN python2 /opt/configparse.py --config /opt/config/config-nova.json --service "/etc/nova/nova.conf"
+RUN python2 /opt/configparse.py --config /opt/config/config-nova-compute.json --service "/etc/nova/nova-compute.conf"
 
 source /opt/osrc-v3
 
@@ -8,12 +23,6 @@ echo "Creating nova-compute service"
 openstack compute service list --service nova-compute
 
 nova-manage cell_v2 discover_hosts --verbose
-
-# echo "Starting nova-compute"
-# /usr/sbin/libvirtd &
-# /usr/sbin/virtlogd &
-# nova-api-metadata --log-file=/var/log/nova/nova-metadata.log &
-# nova-compute --config-file /etc/nova/nova.conf --config-file /etc/nova/nova-compute.conf --log-file=/var/log/nova/nova-compute.log
 
 echo "Starting supervisord"
 supervisord
